1bb9db65d4da360b82d95ff8183ce044b49b545d
Lara Celic <laraa.celic7@gmail.com>
2022-10-20 21:14:04 +0200

    fy mikule

--- frontend/src/components/FAQ.js
+++ frontend/src/components/FAQ.js
@@ -21,27 +21,27 @@ const FAQ = () => {
 
             <Accordion.Item eventKey="2">
                 <Accordion.Header>3. How late can i make my reservation?</Accordion.Header>
-                <Accordion.Body>3hrs at least. We do recommend booking at least 24hrs prior to your need</Accordion.Body>
+                <Accordion.Body>You can make your reservation 3 hours before at least. We do recommend booking at least 24 hours prior to your need</Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="3">
                 <Accordion.Header>4. What happens if my flight is delayed? Complimentary wait time?</Accordion.Header>
-                <Accordion.Body>If your flight is delayed we will wait for you no matter what time you arrive! Also, once flight landed we give up to 2 hrs of waiting time for international flights and 1hr of landing for for domestic</Accordion.Body>
+                <Accordion.Body>If your flight is delayed we will wait for you no matter what time you arrive! Also, once you have arrived we give up to 2 hours of waiting time for international flights and 1 hour for domestic flights.</Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="4">
                 <Accordion.Header>5. Do you provide car seats?</Accordion.Header>
-                <Accordion.Body>Yes, we provide car seats.</Accordion.Body>
+                <Accordion.Body>Yes, we do provide car seats.</Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="5">
                 <Accordion.Header>6. What training requirements do you have for your drivers?</Accordion.Header>
-                <Accordion.Body>All drivers are very carefully selected (drivers are hired with clean record only) and fully licensed and to operate under chauffeur hack license issued by the State of Florida. Also all drivers are fully permitted and fully insured to up to $300.000</Accordion.Body>
+                <Accordion.Body>All drivers are very carefully selected (drivers are hired with clean record only) and fully licensed to operate under chauffeur hack license issued by the State of Florida. Also all drivers are fully permitted and fully insured up to $300.000</Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="6">
                 <Accordion.Header> 7. Do you charge services by mileage or by the hour?</Accordion.Header>
-                <Accordion.Body>We charge per hour and per transfer (from point to point), if hired per hr we do require 3hrs minimum</Accordion.Body>
+                <Accordion.Body>We charge per hour and per transfer (from point to point), if hired per hour we do require 3 hours minimum.</Accordion.Body>
             </Accordion.Item>
 
             <Accordion.Item eventKey="7">
